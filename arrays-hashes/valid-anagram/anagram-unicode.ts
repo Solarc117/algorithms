cc5f39238f1e7b210f0b0c9454dc7be005a3d5ac
solarc117 <jonathan.carlos.bernal@gmail.com>
2023-05-26 13:37:30 -0600

    feat: new versions of anagram alg

--- arrays-hashes/valid-anagram/anagram-unicode.ts
+++
@@ -1,9 +0,0 @@
-// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
-
-interface String {
-  isAnagramOf(string: string): boolean
-}
-
-String.prototype.isAnagramOf = function (string: string): boolean {
-  return true
-}
