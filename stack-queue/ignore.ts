3fb308ab8ba4fbb609cc2ab3fdc5bc3339cdc785
solarc117 <jonathan.carlos.bernal@gmail.com>
2023-07-11 15:08:02 -0600

    feat: :sparkles: progress ;)

---
+++ stack-queue/ignore.ts
@@ -0,0 +1,8 @@
+const hourlyPay = 15.5,
+  hoursPerShift = 4,
+  shiftsPerMonth = 8,
+  monthsInYear = 12
+
+const yearlyPay = hourlyPay * hoursPerShift * shiftsPerMonth * monthsInYear
+
+console.log('yearlyPay:', yearlyPay)
