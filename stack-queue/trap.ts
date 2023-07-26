7f732743f98cb5d7b24a91379d2656944d92b26c
solarc117 <jonathan.carlos.bernal@gmail.com>
2023-07-26 15:40:26 -0600

    feat: progress on water alg

--- stack-queue/trap.ts
+++ stack-queue/trap.ts
@@ -35,8 +35,56 @@ function trap(heights: number[]): number {
   return water
 }
 
-const heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+function trapped(heights: number[]): number {
+  const nextGTE = nextGreaterOrEqual(heights)
+  let water = 0,
+    I = 0
 
-console.log(nextGreaterOrEqual(heights))
+  while (I < heights.length) {
+    const GTE_I = nextGTE.get(I)
+    if (GTE_I === void 0) {
+      I++
+      continue
+    }
 
-// console.log(trap(heights))
+    const distance = GTE_I - I
+    if (distance < 2) {
+      I++
+      continue
+    }
+
+    let rectAreas = 0
+    for (let i = I + 1; i < GTE_I; i++) rectAreas += heights[i]
+    const maxTrapped = (distance - 1) * Math.min(heights[I], heights[GTE_I]),
+      actualTrapped = maxTrapped - rectAreas
+
+    water += actualTrapped
+    I = GTE_I
+  }
+
+  return water
+}
+
+function answer(heights: number[]): number {
+  const stack: number[] = [],
+    top = () => heights[stack.at(-1) as number]
+  let water = 0
+
+  for (let i = 0; i < heights.length; i++) {
+    const current = heights[i]
+
+    while (stack.length > 0 && current >= top()) {
+      const index = stack.pop() as number
+      
+    }
+  }
+}
+
+const heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
+  heights2 = [4, 2, 3]
+
+// console.log(nextGreaterOrEqual(heights))
+
+console.log(trap(heights2))
+
+console.log(trapped(heights2))
